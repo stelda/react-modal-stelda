64a5110a5bdaf953fa54a651e2be29924b0f1a41
stelda <stelda@outlook.fr>
2024-08-04 16:06:17 +0200

    Add Modal component with styling

---
+++ src/Modal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/modal.css';
+
+function Modal({ isOpen, onClose, message }) {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button className="modal-close" onClick={onClose}>
+                    &times;
+                </button>
+                <div className="modal-message">
+                    {message}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
